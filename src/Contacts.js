042ef3dd4784889165a7c13f18cd7496648b4994
MariaJyate <maria.jyate@gmail.com>
2024-05-16 17:46:23 +0200

    contacts

--- src/Contacts.js
+++ src/Contacts.js
@@ -3,8 +3,20 @@ import './contacts.css';
 function Contacts(){
     return(
         <div className="contacts">
-            <div className="textxx">
-                call me beep me if you wanna reach me
+            <div className='cont-wrapper'>
+                <div className="peaks">
+                    Call me, beep me if you wanna reach me
+                </div>
+                <div className='links-wrap'>
+                    <div>Schedule a call ↗</div>
+                    <div>Linkedin ↗</div>
+                    <div>Mail ↗</div>
+                    <div>Git ↗</div>
+                </div>
+                <div className='foot'>
+                    <div>LAST UPDATE: MAY 2024</div>
+                    <div>BACK TO THE WATER BUBBLES ↑</div>
+                </div>
             </div>
         </div>
     )
